0165d1194c450e51f3d003477bdb60d725e096e9
sai shravan <saishravan384@gmail.com>
2023-10-19 20:49:37 +0530

    feat: Create toggle

---
+++ src/toggle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Toggle = () => {
+  return (
+    <label for='toggle'>
+      <input type='checkbox' name='toggle' id='toggle' title='toggle' />
+      <div class='content'></div>
+    </label>
+  )
+}
+
+export default Toggle
