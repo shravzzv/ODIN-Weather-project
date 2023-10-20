098a95770bff99c0ef8077b243205b9cff64bb7e
sai shravan <saishravan384@gmail.com>
2023-10-20 23:20:31 +0530

    feat: Change gradients on each search

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import Form from './components/form'
 import Display from './components/display'
 import Loader from './components/loader'
 import { getCurrentWeather, getForecast } from './apiCall'
+import ErrorDisplay from './components/error'
 
 const root = document.querySelector('#root')
 let currentWeather = await getCurrentWeather('london')
-let forecast
+
+const gradients = [
+  `linear-gradient(to right, #8360c3, #2ebf91)`,
+  `linear-gradient(to right, #ee0979, #ff6a00)`,
+  `linear-gradient(to bottom, #ff4b1f, #1fddff)`,
+  `linear-gradient(to right, #0099f7, #f11712)`,
+  `linear-gradient(to right, #5614b0, #dbd65c)`,
+  `linear-gradient(to right, #757f9a, #d7dde8)`,
+  `linear-gradient(to right, #9796f0, #fbc7d4)`,
+]
+let currentGradient = 0
 
 const handleSubmit = async (e) => {
   e.preventDefault()
@@ -16,13 +27,19 @@ const handleSubmit = async (e) => {
   document.querySelector(`input[type='search']`).placeholder =
     'Search for location'
 
+  // gradients
+  currentGradient === gradients.length - 1
+    ? (currentGradient = 0)
+    : currentGradient++
+
   const query = e.target.elements.location.value.trim()
   if (query) {
     currentWeather = await getCurrentWeather(query)
-    forecast = await getForecast(query, 7)
 
     document.querySelector('.loader').remove()
     root.appendChild(Display(currentWeather))
+    document.querySelector('.display').style.background =
+      gradients[currentGradient]
   } else {
     console.error('Enter a proper location')
   }
@@ -32,3 +49,11 @@ root.appendChild(Form())
 root.appendChild(Display(currentWeather))
 
 document.querySelector('form').addEventListener('submit', handleSubmit)
+
+// todo: Add error handling
+/* 
+Error possibilies:
+1. Incorrect input
+2. 
+
+*/
